a0b5ff24bc0a7542d78b97ea72cd1fc9c8ae61a0
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-01-29 16:45:58 -0400

    `Update src/app.ts and tsconfig.json for new server implementation`

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('hola');
+import { Server } from "./presentation/server";
+(() => {
+  main();
+})();
+
+async function main() {
+  // todo: await base de datos
+  // todo: inicio de nuestro server
+  console.log("main");
+  new Server({}).start();
+}
