3d98facd1585799c1119bebe5b8587ee9c6539ff
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-02-05 13:16:23 -0400

    `feat: added MongoDB connection and configuration`

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config";
+import { MongoDatabase } from "./data/mongodb";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
@@ -7,10 +8,12 @@ import { Server } from "./presentation/server";
 })();
 
 async function main() {
-  // todo: await base de datos
-  // todo: inicio de nuestro server
+  await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  });
   new Server({
     port: envs.PORT,
-    routes: AppRoutes.routes, // todo: routes
+    routes: AppRoutes.routes,
   }).start();
 }
