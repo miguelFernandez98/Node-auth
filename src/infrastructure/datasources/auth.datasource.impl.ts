e130168070a6a9022cc49fc12a3f9de6d71e6602
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-02-05 14:37:34 -0400

    `feat: added bcryptjs dependency and implementation for password hashing and comparison`

--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -1,3 +1,6 @@
+import { BcryptAdapter } from "../../config";
+import { UserModel } from "../../data/mongodb";
+import { UseMapper } from "../mappers/user.mapper";
 import {
   AuthDatasource,
   CustomError,
@@ -5,11 +8,30 @@ import {
   UserEntity,
 } from "../../domain";
 
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
+
 export class AuthDatasourceImpl implements AuthDatasource {
+  constructor(
+    private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+    private readonly compareFunction: CompareFunction = BcryptAdapter.compare
+  ) {}
+
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
     const { name, email, password } = registerUserDto;
     try {
-      return new UserEntity("1", name, email, password, ["ADMIN_ROLE"]);
+      //Verificación de si el email ya existe
+      const exists = await UserModel.findOne({ email });
+      if (exists) throw CustomError.badRequest("User already exists");
+
+      const user = await UserModel.create({
+        name: name,
+        email: email,
+        password: this.hashPassword(password),
+      });
+      await user.save();
+
+      return UseMapper.userEntityFromObject(user);
     } catch (error) {
       if (error instanceof CustomError) {
         throw error;
