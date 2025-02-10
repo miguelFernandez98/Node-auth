c17d69bd2d0d0d25a6e071840e7729e70abdd50f
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-02-10 11:37:45 -0400

    `feat: added JWT_SEED to .env.template and updated config/envs.ts and src/config/jwt.ts accordingly`

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,12 +1,14 @@
 import jwt from "jsonwebtoken";
+import { envs } from "./envs";
 
+const JWT_SEED = envs.JWT_SEED;
 export class JwtAdapter {
   static async generateToken(
     payload: object,
     duration: string | number = "2h"
   ): Promise<string | null> {
     return new Promise((resolve) => {
-      const secret: jwt.Secret = "SEED";
+      const secret: jwt.Secret = JWT_SEED;
       const options: jwt.SignOptions = {
         expiresIn: duration as any,
       };
@@ -19,7 +21,7 @@ export class JwtAdapter {
 
   static validateToken<T>(token: string): Promise<T | null> {
     return new Promise((resolve) => {
-      jwt.verify(token, "SEED", (err, decoded) => {
+      jwt.verify(token, JWT_SEED, (err, decoded) => {
         if (err) return resolve(null);
         resolve(decoded as T);
       });
